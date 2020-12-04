65204d765ed376cd2fb0235dd8b4fb1d32742747
hongzhangming <hongzhangming@foxmail.com>
2020-12-04 17:41:31 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="backGround">
+      <!-- <img src="./assets/backGround.png" width="100%" height="100%" alt="backGround" /> -->
+      <img :src="imgSrc" width="100%" height="100%" alt="backGround" />
+    </div>
+    <div id="YelTop">
+      <YelHead msg="Welcome to YEL App" />
+    </div>
+    <div id="YelLeft"></div>
+    <div id="YelRight"></div>
+    <div id="YelDown">Copyright©top.hongzhangming 版权所有，盗版必究</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import YelHead from "./components/YelHead.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    YelHead,
+  },
+  data() {
+    return {
+      imgSrc: require("./assets/backGround.png"),
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* @import "./css/App.css"; */
+@import url(./css/App.css);
+
+#background {
+  position: absolute;
+  z-index: -1;
+  /* width: 100%; */
+  /* height: 100%; */
+}
+
+#YelTop {
+  position: absolute;
+  z-index: 1;
+  top: 0%;
+  /* background-color: rgb(104, 104, 105); */
+  width: 100%;
+  height: 9%;
+}
+#YelLeft {
+  position: absolute;
+  z-index: 1;
+  left: 0%;
+  top: 9%;
+  width: 77%;
+  height: 98.2%;
+  background-color: rgb(212, 199, 78);
+}
+#YelRight {
+  position: absolute;
+  z-index: 1;
+  right: 0%;
+  top: 9%;
+  width: 23%;
+  height: 98.2%;
+  background-color: rgb(21, 135, 189);
+}
+#YelDown {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 3%;
+  top: 107.2%;
+  background-color: rebeccapurple;
 }
 </style>
---
+++ src/components/YelHead.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="yelHead">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YelHead",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
